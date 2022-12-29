a1bdbc4bd8b962f7c6d90efaad05fd2b8b51b86b
Marie Chanaron <chanaronmarie@gmail.com>
2022-12-29 11:46:57 +0100

    remove the unused useCallback functions from the hooks

--- src/hooks/useFetchReference.js
+++ src/hooks/useFetchReference.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 
 // Function
 import { fetchData } from '../api/fetchData';
--- src/hooks/useFetchVerses.js
+++ src/hooks/useFetchVerses.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 // API
 import { fetchData } from "../api/fetchData";
