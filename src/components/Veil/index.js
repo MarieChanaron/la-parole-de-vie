626186defec31ac5b51586652b0b3eda61fc428a
Marie Chanaron <chanaronmarie@gmail.com>
2022-12-31 23:31:54 +0100

    display the veil on high definition smartphone when the reader form is open

--- src/components/Veil/index.js
+++ src/components/Veil/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { isMobileOnly } from 'react-device-detect';
 
 // Styles
 import './styles.css';
@@ -13,6 +14,7 @@ function Veil({display, displayTable, displayForm, handleClick}) {
       displayform={displayForm} 
       displaytable={displayTable}
       onClick={handleClick} 
+      ismobileonly={isMobileOnly ? "true" : "false"}
     />
   )
 }
