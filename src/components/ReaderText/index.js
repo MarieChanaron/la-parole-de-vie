9ada972f959ef89e5b390ab0d0089e37e6c771a5
Marie Chanaron <chanaronmarie@gmail.com>
2023-01-03 16:13:10 +0100

    test website on tablet

--- src/components/ReaderText/index.js
+++ src/components/ReaderText/index.js
@@ -46,7 +46,7 @@ function ReaderText({text}) {
             if (width > 1200) {
                 setTextHeight(height - 310);
             } else {
-                setTextHeight(height - 310);
+                setTextHeight(height - 294);
             }
         // }
     }, [window.innerWidth]); /* eslint-disable-line react-hooks/exhaustive-deps */
